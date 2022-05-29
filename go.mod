2c1b439a6e25a238a098575307a6b2cc9faa614f
堀航大 <kodenginner4521@gmail.com>
2022-05-29 10:48:57 +0900

    fix_test_code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kod-source/test_github_actions
 
 go 1.18
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,10 +1,19 @@
 package hello
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
 
 func TestGetHello(t *testing.T) {
+	err := godotenv.Load("./../.env")
+	if err != nil {
+		t.Error(err)
+	}
+	expect := os.Getenv("TEST_EXPECT")
 	result := GetHello("テストさん")
-	expect := "こんにちは、テストさん！"
 	if result != expect {
 		t.Error("\n実際：", result, "\n理想：", expect)
 	}
