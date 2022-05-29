ac8494bba79a90fe80ba2046b1a35220dd9e1c95
堀航大 <kodenginner4521@gmail.com>
2022-05-29 11:45:55 +0900

    fix

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,16 +3,13 @@ package hello
 import (
 	"os"
 	"testing"
-
-	"github.com/joho/godotenv"
 )
 
 func TestGetHello(t *testing.T) {
-	err := godotenv.Load()
 	// err := godotenv.Load("./../.env")
-	if err != nil {
-		t.Error(err)
-	}
+	// if err != nil {
+	// 	t.Error(err)
+	// }
 	expect := os.Getenv("TEST_EXPECT")
 	result := GetHello("テストさん")
 	if result != expect {
